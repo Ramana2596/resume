0bf981b340efbbaf93801fc649b56022d09cd39d
Ramana2596 <74821773+Ramana2596@users.noreply.github.com>
2021-02-14 14:58:00 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppMaterialModule } from './app.material.module';
+import { LoginModuleModule } from './login-module/login-module.module';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    LoginModuleModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
